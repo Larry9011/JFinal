3d8ecd51533b567076e0a0ad7e555972c7007b62
Larry <ljq9011@gmail.com>
2014-12-02 14:27:03 +0800

    beetl实现分页

--- src/com/jfinal/controller/BeetlPageController.java
+++ src/com/jfinal/controller/BeetlPageController.java
@@ -48,4 +48,45 @@ public class BeetlPageController extends Controller {
             renderText("Sorry,有些异常，插入失败");
         }
     }
+
+    /**
+     * 按照ID查询记录
+     */
+    public void queryUserById(){
+
+        int id = getParaToInt(0);
+
+        String sql = "select * from t_user where id = ? limit 1";
+        User user = User.dao.findFirst(sql,id);
+        setAttr("user", user);
+        render("edituser.html");
+    }
+
+    /**
+     * 按照ID删除记录
+     */
+    public void deleteById(){
+
+        int id = getParaToInt(0);
+        boolean flag = User.dao.deleteById(id);
+        if(flag){
+            redirect("/page/");//用户浏览器URL重写
+        }else{
+            renderText("Sorry,有些异常，删除失败");
+        }
+    }
+
+    /**
+     * 更新记录
+     */
+    @Before(UserValidator.class)
+    public void updateUser(){
+        User user = getModel(User.class);
+        boolean flag = user.update();
+        if(flag){
+            redirect("/page/");//用户浏览器URL重写
+        }else{
+            renderText("Sorry,有些异常，更新失败");
+        }
+    }
 }
